f42e379b9f670b22d64ea1984a1f447cb9f9bee5
mjevans26 <evans.michael26@gmail.com>
2016-11-02 18:09:12 -0400

    Initial Commit

---
+++ analyses.R
@@ -0,0 +1,82 @@
+#chi square test for distribution of taxonomic groups
+str(chisq.test(group_cont[, 2:3]))
+chisq.test(group_cont[c(1:8,10), 2:3])
+
+plot_ly(filter(group_cont, Group != "Plants and Lichens"), x = ~Group)%>%
+  add_trace(type = "bar", x = ~Group, y = ~RUs.p, name = "R Units")%>%
+  add_trace(type = "bar", x = ~Group, y = ~All.p, name = "All")
+
+#Wilcoxon rank test for difference in median range sizes
+wilcox.test(x = species$count, y = rus$Range, alternative = "l", paired = FALSE)
+wilcox.test(x = species$Area, y = filter(compare, Units>0)$Area, alternative = "l", paired = FALSE)
+
+plot_ly(alpha = 0.75)%>%
+  add_histogram(data = species, x = ~log(Area), histnorm = "probability", name = "All", xbins = list(start = 15, end = 35, size =0.5))%>%
+  add_histogram(data = filter(compare, Units>0), x = ~log(Area), histnorm = "probability", name = "RUs", xbins = list(start = 15, end = 35, size =0.5))%>%
+  layout(barmode = "overlay")
+
+#Regional offices
+all <- group_by(spp_plans, Plan_Lead_Office__FWS_)%>%
+  summarize(plans = n())
+
+us <-filter(compare, Units>0)%>%
+  group_by(Office)%>%
+  summarize(units = n())
+
+office_cont <- left_join(all, us, by = c("Plan_Lead_Office__FWS_"="Office"))
+names(office_cont) <- c("Office", "plans","units")
+office_cont$units[is.na(office_cont$units)]<- 0
+office_cont <- mutate(office_cont, p = units/plans)
+
+chisq.test(office_cont[ ,2:3])
+plot(office_cont$p)
+tanova<-aov(p$units~Office, data = office_cont)
+#temporal trends
+
+#percent of formal consultations RUs vs non
+
+n_formal_ru <- nrow(filter(test, grepl("Formal", consult_type), Scientific%in%compare$Scientific[compare$Units>0]))
+n_ru <- nrow(filter(compare, Units >0))
+
+n_formal <- nrow(filter(test, grepl("Formal", consult_type)))
+n_all <- nrow(plan_dates)
+
+chisq.test(x = c(n_ru, n_all), y = c(n_formal_ru, n_formal))
+
+chisq.test()
+#All
+plot_ly(compare$fcons[compare$Units>0]/compare$cons[compare$Units>0], type = "box")%>%
+  add_trace(y = table(test$Scientific, test$consult_type)[,1]/table(test$Scientific)))
+
+
+#pre-post *only for species with units designated after 2008
+
+#Gila trout vs. Apache Trout
+
+#loggerhead atlantic vs. pacific
+
+#preble's meadow jumping mouse vs. new mexico meadow jumping mouse
+
+#sierra nevada bighorn vs. peninsular bighorn
+
+#desert tortoise vs. gopher tortoise
+
+#california red-legged frog vs. dusky gopher frog 
+
+#Arroyo southwestern toad vs. Houston toad
+
+#Quino checkerspot vs. taylor's checkerspot vs. bay checkerspot
+
+#karner blue vs Lotis blue
+
+#El Segundo blue vs. smith's blue
+
+# robust vs. monterrey spineflower
+
+#indiana vs. gray
+
+#bog vs. ringed map turtle
+
+#northern vs. mexican spotted owl
+
+#bonytail vs. humpback chub
---
+++ processing.R
@@ -0,0 +1,139 @@
+library(dplyr)
+library(stringr)
+library(tidyr)
+
+county_stats <- read.csv("C:/Users/mevans/repos/esasummary/ESAListings/inst/extdata/US_counties_attrib.csv", header = TRUE)
+county_stats$GEOID <- as.character(county_stats$GEOID)
+county_stats$GEOID <- ifelse(nchar(county_stats$GEOID) == 4,
+                             paste0("0", county_stats$GEOID),
+                             county_stats$GEOID)
+
+esacounties <- select(county_stats, ALAND, GEOID)%>%
+  right_join(esacounties, by = "GEOID")
+
+#get total range area for species
+species$Area <- as.vector(group_by(esacounties, Scientific)%>%
+  summarize(area=sum(ALAND))%>%
+  select(Scientific, area)%>%
+  right_join(species, by = "Scientific")%>%
+  select(area))
+
+compare$Office <- as.vector(
+  select(spp_plans, Species_Scientific_Name, Plan_Lead_Office__FWS_)%>%
+    right_join(compare, by = c("Species_Scientific_Name" = "Scientific"))%>%
+    select(Plan_Lead_Office__FWS_)
+)[c(1:28,31:36,39:59),]
+
+
+plan_dates$Common <- sapply(plan_dates$Common_NameScientific_Name, function(x) 
+                                              str_trim(strsplit(x, "\n")[[1]][1])
+       )
+
+plan_dates$Scientific <- sapply(plan_dates$Common_NameScientific_Name, function(x) 
+  str_trim(strsplit(x, "\n")[[1]][4])
+)
+
+dat <- select(plan_dates, Plan_Name, Plan_Date, Scientific)%>%
+  right_join(spp_plans, by = c("Scientific" = "Species_Scientific_Name", "Plan_Name" = "Plan_Title"))
+
+rus <- read.csv("Species.csv", header = TRUE, sep = ",")
+
+compare <- rus
+compare[nrow(compare)+1,c(1:3)]<-c("Zapus hudsonius luteus", "New Mexico meadow jumping mouse", "Mammals")
+compare[nrow(compare)+1,c(1:3)]<-c("Oncorhynchus apache", "Apache trout", "Fish")
+compare[nrow(compare)+1,c(1:3)]<-c("Caretta caretta", "Pacific loggerhead sea turtle", "Reptiles")
+compare[nrow(compare)+1,c(1:3)]<-c("Ovis canadensis nelsoni", "Peninsular bighorn sheep", "Mammals")
+compare[nrow(compare)+1,c(1:3)]<-c("Gopherus polyphemus", "Gopher tortoise", "Reptiles")
+compare[nrow(compare)+1,c(1:3)]<-c("Rana sevosa", "dusky gopher frog", "Amphibians")
+compare[nrow(compare)+1,c(1:3)]<-c("Bufo houstonensis", "Houston toad", "Amphibians")
+compare[nrow(compare)+1,c(1:3)]<-c("Euphydryas editha taylori", "Taylors (=whulge) Checkerspot", "Insects")
+compare[nrow(compare)+1,c(1:3)]<-c("Euphydryas editha bayensis", "Bay checkerspot butterly", "Insects")
+compare[nrow(compare)+1,c(1:3)]<-c("Lycaeides argyrognomon lotis", "Lotis blue butterfly", "Insects")
+compare[nrow(compare)+1,c(1:3)]<-c("Euphilotes enoptes smithi", "Smith's blue butterfly", "Insects")
+compare[nrow(compare)+1,c(1:3)]<-c("Chorizanthe howelii", "Howell's spineflower", "Plants")
+compare[nrow(compare)+1,c(1:3)]<-c("Chorianthe pungens var. hartwegiana", "Ben Lomond spineflower", "Plants")
+compare[nrow(compare)+1,c(1:3)]<-c("Myotis grisescens", "Gray bat", "Mammals")
+compare[nrow(compare)+1,c(1:3)]<-c("Graptemys oculifera", "Ringed map turtle", "Reptiles")
+compare[nrow(compare)+1,c(1:3)]<-c("Strix occidentalis lucida", "Mexican spotted owl", "Birds")
+compare[nrow(compare)+1,c(1:3)]<-c("Gila cypha", "Humpback chub", "Fish")
+
+compare$Units[is.na(compare$Units)] <- 0
+compare$Range[is.na(compare$Range)] <- 
+  
+sapply(1:nrow(compare), function(x, y, z) {
+  if(is.na(y$Region[x])){
+    y$Region[x] <- z$Lead Region[z$Species == y$Scientific[x]]}}, y= compare, z = species_info_table)
+    try(y$Range[x] <- species$count[species$Scientific == y$Scientific[x]])}}, y = compare)
+
+#create 'counties' dataset
+counties<-group_by(esacounties, GEOID)%>%
+  summarise(count = n())
+
+#counties$Species <- sapply(counties$GEOID, function(x,y) y$Scientific[y$GEOID == x], y = esacounties)
+
+counties <- dplyr::left_join(counties, select(county_attrib, GEOID, INTPTLAT, INTPTLON, NAME),by = "GEOID")
+
+
+#create species dataset
+species <- dplyr::group_by(esacounties, Scientific)%>%
+  summarise(count = n())%>%
+  arrange(count)
+
+
+dat$Group <- sapply(dat$Species_Taxonomic_Group, function(x)
+  if(x == "Ferns and Allies"|x == "Flowering Plants"|x == "Conifers and Cycads"|x == "Lichens"){
+    "Plants and Lichens"}
+  else if(x == "Snails"|x=="Clams"){
+    "Molluscs"}else{x})
+
+anti_join(filter(compare, is.na(Region)), spp_plans, by = c("Scientific" = "Species_Scientific_Name"))
+
+#unnest full into rows for each species/consultation
+test <- select(full, activity_code, ESOffice, start_date, timely_concl, consult_type, consult_complex, spp_ev_ls, n_jeop, n_admo, n_nofx)%>%
+  unnest(spp_ev_ls)
+
+test$Scientific <- vapply(1:length(test$Scientific), function(x, y) {
+  s<- strsplit(y[x], "[()]")[[1]]
+  if(length(s) == 2){
+  str_trim(s[2])}else if (length(s) == 4){paste(str_trim(s)[c(2,4)], collapse = " ")}else{""}}, y = test$spp_ev_ls, character(1))
+
+
+compare$Date <- left_join(compare, select(plan_dates, Scientific, Plan_Date), by = "Scientific")[c(1:10,17:34,36:41,43:56),7]
+compare[30,9] <- "08/08/2003"
+compare[39,9] <- "06/29/2010"
+
+compare$admo <- as.data.frame(filter(test,!is.na(n_admo), n_admo>0)%>%
+  group_by(Scientific)%>%
+  summarise(admo = n())%>%
+  select(Scientific, admo)%>%
+  right_join(compare, by = "Scientific")%>%
+  select(admo))
+
+
+for(i in 1:nrow(compare)){
+  if(compare$Units[i]>0){
+  compare$admo[i] <-filter(test, Scientific == compare$Scientific[i], 
+         as.Date(start_date, format = "%Y-%m-%d")>= compare$ymd[i],
+         !is.na(n_admo), n_admo>0)%>%
+        nrow()
+#    group_by(Scientific)%>%
+#    summarise(cons = n())%>%
+#    select(cons)%>%
+#    unlist(use.names=FALSE)
+  }else{
+    compare$admo[i] <-filter(test, Scientific == compare$Scientific[i],
+                              !is.na(n_admo), n_admo>0)%>%
+      nrow()
+#      group_by(Scientific)%>%
+#      summarise(cons = n()) 
+#      select(cons)%>%
+#      unlist(use.names=FALSE)
+  } 
+}
+
+#check for species in consultation df
+for(name in compare$Scientific){
+  if(!(name%in%test$Scientific)){paste(print(name%in%test$Scientific), print(name), sep = " ")}}
+
+for(name in us$Office){
+  if(!(name%in%office_cont$Office)){paste(print(name%in%office_cont$Office), print(name), sep = " ")}}
